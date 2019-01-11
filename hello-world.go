e9b878a29a1d3c57c68a523e5d89e726573b38c7
Chris Smith <chris.smith@deciphernow.com>
2019-01-11 14:41:21 -0500

    see if the patch works through the workflow

--- hello-world.go
+++ hello-world.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello World! And now with a patch!"
+		response = "Hello World! Maybe this is the commit that actually works!"
 	}
 
 	fmt.Fprintln(w, response)
