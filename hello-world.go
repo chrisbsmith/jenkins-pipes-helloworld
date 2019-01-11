e54ce208538927c9e3ca68efee0cedcb631316b3
Chris Smith <chris.smith@deciphernow.com>
2019-01-11 12:32:12 -0500

    updated dc

--- hello-world.go
+++ hello-world.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello World! Are we making progress yet? New commit now."
+		response = "Hello World! Updated deployment strategy."
 	}
 
 	fmt.Fprintln(w, response)
